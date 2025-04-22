12f417c6721889b7c157a3ef72616094b0956566
Cesar Carrasco <ccarrascocarre@gmail.com>
2025-04-21 22:15:27 -0400

    Se genera el archivo Paciente.java el cual permite trasformar una clase a una entidad de la base de datos

---
+++ src/main/java/com/duoc/hospital/hospital/models/Paciente.java
@@ -0,0 +1,48 @@
+package com.duoc.hospital.hospital.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+/**
+ * @Entity -> Nos permite definir que esta tabla estara almacenada en la BD que se declara en el properties
+ * @Table -> Nos permite setear el nombre de la tabla con la que trabajaremos.
+ * @Getter -> Se generan todos getters de la clase automaticamente
+ * @Setter -> Se generan todos los seter de la clase automaticamente
+ */
+@Entity
+@Table(name = "patients")
+@Getter @Setter @NoArgsConstructor @ToString
+public class Paciente {
+
+    /**
+     * @Id -> Se define como la llave primaria del elemento
+     * @GeneratedValue -> Nos permite generar el valor de id de forma automatica
+     */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "paciente_id")
+    private Long pacienteId;
+
+
+    @Column(unique = true, nullable = false)
+    private String run;
+
+    @Column(nullable = false)
+    private String nombres;
+
+    @Column(nullable = false)
+    private String apellidos;
+
+    @Column(name ="fecha_nacimiento")
+    private Date fechaNacimiento;
+
+    @Column(nullable = false, unique = true)
+    private String correo;
+
+}
