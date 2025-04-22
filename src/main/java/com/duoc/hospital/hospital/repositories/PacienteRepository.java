0868a2806b45f2f866084e063329d12b2471f5b8
Cesar Carrasco <ccarrascocarre@gmail.com>
2025-04-21 23:28:46 -0400

    Se agregan metodos de busqueda personalizados tales como findByRun y findByCorre

--- src/main/java/com/duoc/hospital/hospital/repositories/PacienteRepository.java
+++ src/main/java/com/duoc/hospital/hospital/repositories/PacienteRepository.java
@@ -4,10 +4,17 @@ import com.duoc.hospital.hospital.models.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * @Repository -> Nos permite definir este elemento del tipo repository
  */
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 
+    // Metodo que me permite realizar la busqueda de un paciente mediante el rut
+    Optional<Paciente> findByRun(String run);
+
+    // Metodo que me permite realizar la busqueda de un paciente mediante el correo
+    Optional<Paciente> findByCorreo(String correo);
 }
