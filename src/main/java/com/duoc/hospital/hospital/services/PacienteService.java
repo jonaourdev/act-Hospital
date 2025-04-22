2408c20ae653320416fba6421fbbb53f89bd5d8b
Cesar Carrasco <ccarrascocarre@gmail.com>
2025-04-21 23:38:02 -0400

    Se genera el service del paciente

---
+++ src/main/java/com/duoc/hospital/hospital/services/PacienteService.java
@@ -0,0 +1,37 @@
+package com.duoc.hospital.hospital.services;
+
+import com.duoc.hospital.hospital.models.Paciente;
+
+import java.util.List;
+
+/**
+ * ¿Porqué se genera Paciente Service como interfaz?
+ *
+ * 1. Principio de programación orientada a interfaces (SOLID)
+ * Se basa en el principio de inversión de dependencias (D) del modelo SOLID.
+ * Esto permite que otras capas (como el controller o incluso otros servicios) dependan de abstracciones (interfaces) y
+ * no de implementaciones concretas (ServiceImpl).
+ *
+ * 2. Facilita el testing
+ * Cuando haces tests unitarios, puedes mockear fácilmente la interfaz en lugar de la implementación.
+ *
+ * 3. Posibilidad de múltiples implementaciones
+ * Si en algún momento necesitas dos formas distintas de implementar una lógica, puedes tener múltiples ServiceImpl y
+ * elegir cuál inyectar según el contexto (por ejemplo, por perfil de Spring o por uso de @Qualifier).
+ *
+ * 4. Mejora el desacoplamiento
+ * El Controller no necesita saber cómo funciona el servicio, solo qué métodos ofrece.
+ * Si el día de mañana cambias la lógica interna (por ejemplo, pasar de MySQL a Mongo), no afecta al controller.
+ *
+ */
+
+public interface PacienteService {
+
+    List<Paciente> findAll();
+    Paciente findById(Long id);
+    Paciente save(Paciente paciente);
+    void deleteById(Long id);
+    Paciente updateById(Long id, Paciente paciente);
+    Paciente findByCorreo(String correo);
+    Paciente findByRun(String run);
+}
---
+++ src/main/java/com/duoc/hospital/hospital/services/PacienteServiceImpl.java
@@ -0,0 +1,77 @@
+package com.duoc.hospital.hospital.services;
+
+import com.duoc.hospital.hospital.exceptions.PacienteException;
+import com.duoc.hospital.hospital.models.Paciente;
+import com.duoc.hospital.hospital.repositories.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PacienteServiceImpl implements PacienteService {
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @Override
+    public List<Paciente> findAll() {
+        return pacienteRepository.findAll();
+    }
+
+    @Override
+    public Paciente findById(Long id) {
+        // Se genera metodo que permite realizar la busqueda de un usuario con el id, en caso de no encontrar un resul
+        // Se lanza una excepcion que tiene que ver con paciente.
+        return pacienteRepository.findById(id).orElseThrow(
+                () -> new PacienteException("Paciente con id " + id + " no encontrado")
+        );
+    }
+
+    @Override
+    public Paciente save(Paciente paciente) {
+        // Se realiza la validacion que el usuario con el RUT enviado no exista previamente en nuestra base de datos
+        if (pacienteRepository.findByRun(paciente.getRun()).isPresent()) {
+            throw new PacienteException("Paciente con Run "+paciente.getRun()+" ya existente");
+        }
+        // Se realiza la validacion que el usuario con el Correo enviado no exista previamente en nuestra base de datos
+        if (pacienteRepository.findByCorreo(paciente.getCorreo()).isPresent()) {
+            throw new PacienteException("Paciente con Correo "+paciente.getCorreo()+" ya existente");
+        }
+        return pacienteRepository.save(paciente);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        pacienteRepository.deleteById(id);
+    }
+
+    @Override
+    public Paciente updateById(Long id, Paciente paciente) {
+        // Metosdo lamda que nos permite generar la actualizacion de un elemento a traves del metodo MAP en caso de
+        // que se encuentre un valor, en caso contrario se genera la excpecion del caso
+        return pacienteRepository.findById(id).map(p->{
+            p.setNombres(paciente.getNombres());
+            p.setApellidos(paciente.getApellidos());
+            p.setCorreo(paciente.getCorreo());
+            p.setFechaNacimiento(paciente.getFechaNacimiento());
+            p.setRun(paciente.getRun());
+            return pacienteRepository.save(p);
+        }).orElseThrow(
+                () -> new PacienteException("Paciente con id " + id + " no encontrado")
+        );
+    }
+
+    @Override
+    public Paciente findByCorreo(String correo) {
+        return pacienteRepository.findByCorreo(correo).orElseThrow(
+                () -> new PacienteException("Paciente con Correo "+correo+" no encontrado")
+        );
+    }
+
+    @Override
+    public Paciente findByRun(String run) {
+        return pacienteRepository.findByRun(run).orElseThrow(
+                () -> new PacienteException("Paciente con Run "+run+" no encontrado")
+        );
+    }
+}
