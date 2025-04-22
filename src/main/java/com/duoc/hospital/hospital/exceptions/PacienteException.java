610a8c249b8e1743bf06f1e369ca711f1586ad8c
Cesar Carrasco <ccarrascocarre@gmail.com>
2025-04-21 23:28:10 -0400

    Se genera la excepcion de la clase PacienteException.java

---
+++ src/main/java/com/duoc/hospital/hospital/exceptions/PacienteException.java
@@ -0,0 +1,7 @@
+package com.duoc.hospital.hospital.exceptions;
+
+public class PacienteException extends RuntimeException {
+    public PacienteException(String message) {
+        super(message);
+    }
+}
