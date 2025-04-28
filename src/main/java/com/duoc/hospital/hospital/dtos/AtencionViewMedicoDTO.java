58d953a141bd017e409efe10c7e56e2953f122c8
Cesar Carrasco <ccarrascocarre@gmail.com>
2025-04-27 21:29:41 -0400

    Se generar DTOs especificas para cada uno de los extremos de la asociación.

---
+++ src/main/java/com/duoc/hospital/hospital/dtos/AtencionViewMedicoDTO.java
@@ -0,0 +1,39 @@
+package com.duoc.hospital.hospital.dtos;
+
+import com.duoc.hospital.hospital.models.Paciente;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class AtencionViewMedicoDTO {
+    private Long atencionId;
+    private LocalDateTime horaAtencion;
+    private Integer costo;
+    private String comentario;
+    private String runMedico;
+    private String nombreMedico;
+    private String runPaciente;
+    private String nombrePaciente;
+
+    public AtencionViewMedicoDTO(
+            Long atencionId,
+            LocalDateTime horaAtencion,
+            String comentario,
+            Integer costo,
+            Paciente paciente
+    ){
+        this.atencionId = atencionId;
+        this.horaAtencion = horaAtencion;
+        this.comentario = comentario;
+        this.costo = costo;
+        this.nombrePaciente = paciente.getNombres() + paciente.getApellidos();
+        this.runPaciente = paciente.getRun();
+    }
+}
---
+++ src/main/java/com/duoc/hospital/hospital/dtos/AtencionViewPacienteDTO.java
@@ -0,0 +1,39 @@
+package com.duoc.hospital.hospital.dtos;
+
+import com.duoc.hospital.hospital.models.Medico;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class AtencionViewPacienteDTO {
+    private Long atencionId;
+    private LocalDateTime horaAtencion;
+    private Integer costo;
+    private String comentario;
+    private String runMedico;
+    private String nombreMedico;
+    private String runPaciente;
+    private String nombrePaciente;
+
+    public AtencionViewPacienteDTO(
+            Long atencionId,
+            LocalDateTime horaAtencion,
+            String comentario,
+            Integer costo,
+            Medico medico
+    ){
+        this.atencionId = atencionId;
+        this.horaAtencion = horaAtencion;
+        this.comentario = comentario;
+        this.costo = costo;
+        this.runMedico = medico.getRun();
+        this.nombreMedico = medico.getNombreCompleto();
+    }
+}
